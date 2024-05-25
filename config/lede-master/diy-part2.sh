4b37d884de21e70b26e35df4cdd4dd3d9a9a9f1d
sa979843085 <61696749+sa979843085@users.noreply.github.com>
2024-05-25 17:19:00 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -25,12 +25,12 @@ sed -i 's/192.168.1.1/192.168.100.100/g' package/base-files/files/bin/config_gen
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
-# rm -rf feeds/packages/net/mosdns
-# rm -rf feeds/packages/net/msd_lite
-# rm -rf feeds/packages/net/smartdns
+# 主题
 rm -rf feeds/luci/themes/luci-theme-argon
-# rm -rf feeds/luci/applications/luci-app-mosdns
+# 软件
 rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-ddns-go
+
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
